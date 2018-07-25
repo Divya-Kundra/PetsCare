8c6ec8b2764f02a278440f0d46e367a99b3e561a
selman birinci <selmanbirinci61@gmail.com>
2018-07-25 13:01:42 -0400

    I added main page for the application. I only add the buttons. I am going to add setup bar and constrains tomorrow. so we will have 6 activity to finish after that point.

---
+++ app/src/main/java/com/example/testuser/petscradle/Main_Page.java
@@ -0,0 +1,13 @@
+package com.example.testuser.petscradle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main__page);
+    }
+}
